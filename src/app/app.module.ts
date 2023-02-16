83fa9a8a22b1ba330c00d7980462af473d9cc022
Samir Makhani <samir@kinnect.us>
2023-02-16 15:46:23 +0530

    main

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './child-component/header/header.component';
+import { PreLoaderComponent } from './child-component/pre-loader/pre-loader.component';
+import { FooterComponent } from './child-component/footer/footer.component';
+import { ContactComponent } from './child-component/contact/contact.component';
+import { BlogComponent } from './child-component/blog/blog.component';
+import { PricingComponent } from './child-component/pricing/pricing.component';
+import { WorkComponent } from './child-component/work/work.component';
+import { CallToActionComponent } from './child-component/call-to-action/call-to-action.component';
+import { ServiceComponent } from './child-component/service/service.component';
+import { AboutComponent } from './child-component/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PreLoaderComponent,
+    FooterComponent,
+    ContactComponent,
+    BlogComponent,
+    PricingComponent,
+    WorkComponent,
+    CallToActionComponent,
+    ServiceComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
